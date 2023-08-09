dc63a6a27061d826f7221206aa8eecfb94ad81e9
matthewharrilal <matthewharrilal@gmail.com>
2023-08-09 08:57:13 -0700

    Removed trailing whitespace

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) 
+/* This method simply prints string out using printf  */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
