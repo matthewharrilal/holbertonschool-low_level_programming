4d2b5ad858ae5ec77bb09f18b0ea0d83f4240a6e
matthewharrilal <matthewharrilal@gmail.com>
2023-08-09 09:41:49 -0700

    Changed import statement

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
 	char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19";
-	/* Write to standard output the message and the size of the message represented by an array */
 	
+	/* Write to standard output the message and the size of the message represented by an array */
 	write(1, message, sizeof(message) - 1);
 	return (0);
 }
