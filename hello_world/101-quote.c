f74f2c07e02438bc3e587f2a9cd6a15ed92a2d14
matthewharrilal <matthewharrilal@gmail.com>
2023-08-09 09:43:50 -0700

    Fixed linter warnings

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -4,8 +4,7 @@
 int main(void)
 {
 	char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19";
-	
 	/* Write to standard output the message and the size of the message represented by an array */
 	write(1, message, sizeof(message) - 1);
-	return (0);
+	return (1);
 }
