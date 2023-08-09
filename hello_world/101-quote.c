21a5b0857cac8aca6254f534b32a352a262fb069
matthewharrilal <matthewharrilal@gmail.com>
2023-08-09 09:39:42 -0700

    Added a script that uses write to stdout a message instead of print f

---
+++ hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+	/* Write to standard output the message and the size of the message represented by an array */
+	
+	write(1, message, sizeof(message) - 1);
+	return (0);
+}
