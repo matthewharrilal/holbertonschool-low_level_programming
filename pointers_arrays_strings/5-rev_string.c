71a503b8075973273bbebdc40db4fc468c05ba1f
matthewharrilal <matthewharrilal@gmail.com>
2023-08-10 10:42:13 -0700

    Testing

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,6 +12,6 @@ void rev_string(char *s)
 
 	for (iterator = lengthOfString - 1; iterator >= 0; iterator--)
 	{
-		s[iterator] = s[lengthOfString - iterator];
+		printf("%s", s[lengthOfString - iterator]);
 	}
 }
