bb9a7d1285dfe91e26bc164e55e3ceae72bce29e
matthewharrilal <matthewharrilal@gmail.com>
2023-08-10 10:07:42 -0700

    Printing a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include <stdio.h>
+
+void print_rev(char *s)
+{
+	int lengthOfString;
+
+	lengthOfString = strlen(s);
+
+	for (int i = lengthOfString - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+}
