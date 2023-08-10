b17bb062a753368145468bdb1900b06eb09f8088
matthewharrilal <matthewharrilal@gmail.com>
2023-08-10 07:03:46 -0700

    Added method prototype and function body

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+#include <stdio.h>
+
+void print_alphabet_x10(void)
+{
+	int counter;
+	char letter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			putchar(letter);
+			putchar("\n")
+		}
+	}
+}
