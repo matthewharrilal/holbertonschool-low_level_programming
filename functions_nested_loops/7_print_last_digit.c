70cd750781d0e903c3dea9d246406b1eb667cbc2
matthewharrilal <matthewharrilal@gmail.com>
2023-08-10 07:40:58 -0700

    Returning last digit of int

---
+++ functions_nested_loops/7_print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+#include <stdio.h>
+
+int print_last_digit(int)
+{
+	char str[200]; 
+	sprintf(str, "%d", int);
+
+	lengthOfString = strlen(int);
+	return str[lengthOfString - 1] - '0';
+}
