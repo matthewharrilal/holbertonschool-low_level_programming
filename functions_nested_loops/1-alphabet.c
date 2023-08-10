9b7d28ce0159899a51c5b7cb3e51f3cfcdd9679c
matthewharrilal <matthewharrilal@gmail.com>
2023-08-10 06:55:24 -0700

    Pushing main header file and alphabet method

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}	
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 int _putchar(void);
+void print_alphabet(void);
+
+#endif
