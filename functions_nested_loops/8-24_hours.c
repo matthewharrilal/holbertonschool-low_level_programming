73e6ae4ce8c2eaf42459c3de69da2061f70ed944
matthewharrilal <matthewharrilal@gmail.com>
2023-08-10 08:37:49 -0700

    Added jack bauer clock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 #include <stdio.h>
 
 void jack_bauer(void)
@@ -12,30 +12,31 @@ void jack_bauer(void)
 
 	char concatenatedString[10];
 
-	for (hourCounter = 0; hourCounter < 24; hourCounter ++)
+	for (hourCounter = 0; hourCounter < 24; hourCounter++)
 	{
-		for (minuteCounter = 0; minuteCounter < 60; minuteCounter ++)
+		for (minuteCounter = 0; minuteCounter < 60; minuteCounter++)
 		{
 			if (minuteCounter < 10)
 			{
-				minuteString = snprintf(minuteString, sizeof(minuteString), "0%d", minuteCounter);
+				sprintf(minuteString, "0%d", minuteCounter);
 			} else 
 			{
-				minuteString = snprintf(minuteString, sizeof(minuteString), "%d", minuteCounter);
+				sprintf(minuteString, "%d", minuteCounter);
 			}
 
 			
 			if (hourCounter < 10)
 			{
-				hourString = snprintf(hourString, sizeof(hourString), "0%d:", hourCounter);
+				sprintf(hourString, "0%d:", hourCounter);
 			} else 
 			{
-				hourString = snprintf(hourString, sizeof(hourString), "%d:", hourCounter);
+				sprintf(hourString, "%d:", hourCounter);
 			}
 
-			concatenatedString = strcat(hourString, minuteString);
-			printf(concatenatedString);
+			strcpy(concatenatedString, hourString);
+			strcat(concatenatedString, minuteString);
+			printf("%s\n", concatenatedString);
 		}
 	}
 }
-
+i
