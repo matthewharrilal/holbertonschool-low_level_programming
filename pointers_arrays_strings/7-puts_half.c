8ea07df1e8fde78edf958d3315964a88581c0a44
matthewharrilal <matthewharrilal@gmail.com>
2023-08-10 12:18:31 -0700

    Added colon

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 	size_t i; 
 	int lengthOfString;
 
-	lengthOfString = strlen(str)
+	lengthOfString = strlen(str);
 
 	for (i = lengthOfString - 1; i > (lengthOfString / 2); i--)
 	{
