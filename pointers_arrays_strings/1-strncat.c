3a27b4046462db055a8ced141f7edea5936625a1
matthewharrilal <matthewharrilal@gmail.com>
2023-08-11 12:32:25 -0700

    Fixed implementation

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -6,10 +6,15 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int index;
+	size_t sourceLength;
 	size_t destinationLength;
+	size_t combinedLength;
+
 	destinationLength = strlen(dest);
+	sourceLength = strlen(src);
+	combinedLength = destinationLength + sourceLength;
 	
-	for (index = destinationLength; index <= n; index++)
+	for (index = destinationLength; index <= ((int)destinationLength - 1 + n); index++)
 	{
 		dest[index] = src[index - destinationLength];
 	}
