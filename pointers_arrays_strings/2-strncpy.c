bee310373a7b9c4874c2b0f0fb220fdabd1cd6d2
matthewharrilal <matthewharrilal@gmail.com>
2023-08-11 13:04:27 -0700

    Removed unsused variable

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -6,10 +6,6 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index;
-	size_t destinationLength;
-
-	destinationLength = strlen(dest);
-
 	for (index = 0; index <= n - 1; index++)
 	{
 		dest[index] = src[index];
