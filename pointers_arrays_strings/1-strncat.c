7d2d8861b53d3b2ace9e2265bc08864caeed0bac
matthewharrilal <matthewharrilal@gmail.com>
2023-08-11 12:38:31 -0700

    Removed sourceLength

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -6,11 +6,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int index;
-	size_t sourceLength;
 	size_t destinationLength;
 
 	destinationLength = strlen(dest);
-	sourceLength = strlen(src);
 	
 	for (index = destinationLength; index <= ((int)destinationLength - 1 + n); index++)
 	{
