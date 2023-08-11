4682e2f49c71ad43bb71efb30bc752e323d4e1fd
matthewharrilal <matthewharrilal@gmail.com>
2023-08-11 12:47:43 -0700

    Strcopy?

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <string.h>
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+	size_t destinationLength;
+	int combinedLength;
+
+	destinationLength = strlen(dest);
+
+	for (index = destinationLength; index <= (int)destinationLength - 1 + n; index++)
+	{
+		dest[index] = src[index - destinationLength];
+	}	
+	return dest
+}
