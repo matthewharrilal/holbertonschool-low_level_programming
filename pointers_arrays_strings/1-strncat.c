f43032cf126a621b7ec1ea1589e795b916d56add
matthewharrilal <matthewharrilal@gmail.com>
2023-08-11 12:35:43 -0700

    Removed variable declaration

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,6 @@ char *_strncat(char *dest, char *src, int n)
 	int index;
 	size_t sourceLength;
 	size_t destinationLength;
-	size_t combinedLength;
 
 	destinationLength = strlen(dest);
 	sourceLength = strlen(src);
@@ -20,4 +19,4 @@ char *_strncat(char *dest, char *src, int n)
 
 	return dest;
 
-}
+:}
