ea0cae1c00422aff275a81d4a7e6acb1368bd586
matthewharrilal <matthewharrilal@gmail.com>
2023-08-11 09:20:13 -0700

    Want to reference the pointer not dereference the pointer

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int index;
 
-	for (index = 0; src[index] != '\0'; index++)
+	for (index = 0; src[index] == '\0'; index++)
 	{
 		dest[index] = src[index];
 	}
