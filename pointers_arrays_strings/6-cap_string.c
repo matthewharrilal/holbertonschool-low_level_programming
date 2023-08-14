5a1f2afb3710490b34ac8a735548bc6da79d6e77
matthewharrilal <matthewharrilal@gmail.com>
2023-08-14 08:50:53 -0700

    Added logic for captializing first of every word

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+#include <stdio.h>
+
+char *cap_string(char *c)
+{
+	bool foundSpace;
+
+        foundSpace = false;
+        while (*c != '\0')
+        {
+                if (foundSpace == true)
+                {
+                        if ('a' <= *c && *c <= 'z')
+                        {
+                                *c = *c - ('a' - 'A');
+                        }
+
+                        foundSpace = false;
+                }
+
+                if (*c == ' ')
+                {
+                        foundSpace = true;
+                }
+
+                c++;
+        }
+        return (c);
+}
