67abc5f7ff7fb6c6972c368310133e10f8d1a43a
matthewharrilal <matthewharrilal@gmail.com>
2023-08-14 07:51:22 -0700

    Added program that reverses array contents

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+#include <stdio.h>
+
+void reverse_array(int *a, int n)
+{
+	int digits[n];
+	int index;
+
+	index = 0;
+
+	int *copy;
+    	copy = (int *)malloc(n * sizeof(int));
+  
+
+	int copyIndex;
+	
+	for (copyIndex = 0; copyIndex < n; copyIndex++)
+	{
+		copy[copyIndex] = a[copyIndex];
+	}
+	
+	do 
+	{
+                *a = copy[(n - 1) - index];
+                index++;
+                a++;
+	} while (index < n);
+}
