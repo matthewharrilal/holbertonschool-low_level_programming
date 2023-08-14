35d56054530ed6a2694bf4b18483531dfaec06ce
matthewharrilal <matthewharrilal@gmail.com>
2023-08-14 12:31:12 -0700

    Copying memory of an object to another object

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include <stdio.h>
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index <= n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	return dest;
+}
