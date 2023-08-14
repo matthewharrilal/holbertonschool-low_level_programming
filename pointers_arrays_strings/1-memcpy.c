d7fed8f4aea8cec201c9d3664d8d728e2946dc54
matthewharrilal <matthewharrilal@gmail.com>
2023-08-14 12:43:00 -0700

    Changed comparison operator

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 
-	for (index = 0; index <= n; index++)
+	for (index = 0; index < n; index++)
 	{
 		dest[index] = src[index];
 	}
