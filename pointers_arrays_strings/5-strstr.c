43a9745287107fa0a5303b53881150ba39d756e8
matthewharrilal <matthewharrilal@gmail.com>
2023-08-15 11:46:59 -0700

    Added program looking for substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char *_strstr(char *haystack, char *needle)
+{
+	int needleLength;
+	int counter;
+
+	bool startedSequence;
+
+	char firstChar;
+	char *startingPos;
+	char *startingNeedlePos;
+
+	firstChar = *needle;
+	needleLength = strlen(needle);
+	counter = 0;
+	startingNeedlePos = needle;
+
+	while (*haystack && *needle)
+	{	
+		if (*haystack == firstChar && strchr(needle, *haystack))
+		{	
+			startedSequence = true;
+			startingPos = haystack;
+		}
+
+		while (startedSequence && *haystack)
+		{
+			if (*haystack == *needle)
+			{
+				counter++;
+				
+			} else
+			{
+				startedSequence = false;
+				needle = startingNeedlePos - 1;
+	
+				if (counter != needleLength)
+				{
+					counter = 0;
+				}
+			}
+
+			haystack++;
+			needle++;
+		}
+		haystack++;
+	}
+	
+	if (startedSequence || (counter == needleLength))
+	{
+		return startingPos;
+	} else
+	{
+		return NULL;
+	}
+	
+}
