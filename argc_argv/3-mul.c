d0eee4320f298b705ac30a8e1aa5fd1fd0d46838
matthewharrilal <matthewharrilal@gmail.com>
2023-08-16 12:00:27 -0700

    Getting product of two args

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int product;
+	if ((argc - 1) < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	product = argv[1] * argv[2];
+	printf("%d\n", product);
+	return (0);
+}
