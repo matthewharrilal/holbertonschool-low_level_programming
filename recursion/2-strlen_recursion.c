6e1e581531ab3ec77303e649ec15e2ae8d3b3978
matthewharrilal <matthewharrilal@gmail.com>
2023-08-16 07:26:54 -0700

    Testing omitting return keyword

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,16 +4,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int counter;
-
-	if (counter == NULL)
-	{
-		counter = 0;
-	}
-
 	if (*s != '\0')
 	{
-		_strlen_recursion(++s);
-		return counter++;
+		1 + _strlen_recursion(++s);
 	}
 }
