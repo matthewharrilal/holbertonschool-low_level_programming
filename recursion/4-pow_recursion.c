505ec21eb2c04aaae2810b60779a95bee24c1574
matthewharrilal <matthewharrilal@gmail.com>
2023-08-16 08:00:51 -0700

    Added program that squares number to yth degreee recrusively

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	} else if (y == 1)
+	{
+		return (x);
+	} else if (y == 0)
+	{
+		return (1);
+	}
+
+	return x * _pow_recursion(x, y - 1);
+}
