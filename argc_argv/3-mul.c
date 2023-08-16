fe8b15a996cd0eb2791dfea7554ab0b1fb2a457a
matthewharrilal <matthewharrilal@gmail.com>
2023-08-16 12:09:44 -0700

    Correctly de-referencing need to convert chars to int

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	int product;
+	int firstInt;
+	int secondInt;
+
 	if ((argc - 1) < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	
-	product = *argv[1] * *argv[2];
+	/* We are correctly de-referencing need to convert chars to int now  */
+	firstInt = atoi(*argv[1]);
+	secondInt = atoi(*argv[2]);
+	product = firstInt * secondInt;
+
 	printf("%d\n", product);
 	return (0);
 }
