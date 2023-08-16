395a9afaf194663098433d0bd1eaa7db06d5b9bf
matthewharrilal <matthewharrilal@gmail.com>
2023-08-16 10:39:47 -0700

    Iterating through current directory and compiling all c files into object files then archiving

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -2,4 +2,10 @@
 
 current_directory="$PWD"
 
+for c_file in "$current_directory"/*.c; do
+	base_name=$(basename "$c_file" .c)
+	object_file="$base_name.o"
+	gcc -c "$c_file" -o "$object_file"
+done
+
 ar -rc liball.a "$current_directory"/*.c 
