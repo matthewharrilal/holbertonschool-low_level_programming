3c26c26c48a7c6802e61471fb39a9f0ca35024b0
matthewharrilal <matthewharrilal@gmail.com>
2023-08-17 06:46:41 -0700

    Added program that dynamically alloc memory

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return NULL;
+	}
+
+	char *characters;
+	characters = (char *)malloc(size * sizeof(char));
+	return characters;
+}
