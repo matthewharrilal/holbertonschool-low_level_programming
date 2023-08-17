8f65986db61ba70007ebb9802159cec3b946a1f4
matthewharrilal <matthewharrilal@gmail.com>
2023-08-17 10:37:12 -0700

    Potentially freeing the grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void free_grid(int **grid, int height)
+{
+	int heightIndex;
+
+	for (heightIndex = 0; heightIndex < height; heightIndex++)
+	{
+		free(*grid[heightIndex]);
+	}
+}
