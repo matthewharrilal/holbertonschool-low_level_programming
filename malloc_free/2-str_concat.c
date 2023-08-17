a1e323442483640ee9463fe50e2d672c61104c09
matthewharrilal <matthewharrilal@gmail.com>
2023-08-17 08:39:15 -0700

    Using realloc correctly?

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t firstSize;
+	size_t secondSize;
+	size_t index;
+	size_t secondIndex;
+
+	if (*s1 == NULL)
+	{
+		*s1 = "";
+	}
+
+	if (*s2 == NULL)
+	{
+		*s2 = NULL;
+	}
+
+	while (s1[firstSize])
+	{
+		firstSize++;
+	}
+
+	while (s2[secondSize])
+	{
+		secondSize++;
+	}
+
+	*s1 = (char *)realloc(s1, (firstSize + secondSize) * sizeof(char));
+	secondIndex = 0;
+	for (index = firstSize, index <= (firstSize + secondSize); index++)
+	{
+		s1[index] = s2[secondIndex]
+		secondIndex++;
+	}
+}
