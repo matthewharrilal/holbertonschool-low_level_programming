053fce869a769856f127b0bccd718d9c55285a4a
matthewharrilal <matthewharrilal@gmail.com>
2023-08-17 07:27:32 -0700

    Duplicating string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+#include <stdio.h>
+
+char *_strdup(char *str)
+{
+	char *copy;
+	size_t size;
+
+	if (str == NULL)
+	{
+		return NULL;
+	}
+
+	while (*str)
+	{
+		size++;
+		str++;
+	}
+
+	copy = (char *)malloc(size * sizeof(char));
+
+	if (copy == NULL)
+	{
+		return NULL;
+	}
+
+	return copy;
+}
