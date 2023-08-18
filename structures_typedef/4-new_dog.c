8b97e1bf6f8896fd8b263c2f84a7da512bbceab0
matthewharrilal <matthewharrilal@gmail.com>
2023-08-18 09:48:52 -0700

    Added duplicates of both name and owner

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,17 +6,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogObject;
+	char *nameCopy;
+	char *ownerCopy;
 
 	dogObject = (dog_t *)malloc(sizeof(dog_t));
+
+	nameCopy = name;
+	ownerCopy = owner;
 
 	if (dogObject == NULL)
 	{
 		return NULL;
 	}
 
-	dogObject->name = name;
+	dogObject->name = nameCopy;
 	dogObject->age = age;
-	dogObject ->owner = owner;
+	dogObject ->owner = ownerCopy;
 
 	return dogObject;
 }
