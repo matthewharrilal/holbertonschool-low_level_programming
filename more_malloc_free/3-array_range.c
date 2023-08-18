3cd67b33f0102e97e4dbf7fa17bed6fcdcee2232
matthewharrilal <matthewharrilal@gmail.com>
2023-08-18 06:47:21 -0700

    Inserted integers in given min - max range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+	int *collectionPointer;
+	int index;
+	int secondaryIndex;
+
+	if (min > max)
+	{
+		return NULL;
+	}
+
+	collectionPointer = (int *)malloc((max - min + 1) * sizeof(int));
+
+	secondaryIndex = 0;
+	for (index = min; index < (max - min + 1))
+	{
+		collectionPointer[secondaryIndex] = index;
+		secondaryIndex ++;
+	}
+
+	return collectionPointer;
+}
+
+
