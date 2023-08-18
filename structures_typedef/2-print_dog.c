0640f69403546b9ab2ec6dd69640270ad5dda2cc
matthewharrilal <matthewharrilal@gmail.com>
2023-08-18 09:20:24 -0700

    Fixed syntax

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,6 +10,6 @@ void print_dog(struct dog *d)
 	}
 
 	printf(d->name != NULL ? "Name: %s\n": "Name: (nil)", d->name);
-	printf(d->age != NULL ? "Age: %d\n" : "Age: (nil)", d->age);
+	printf(d->age != 0 ? "Age: %.2f\n" : "Age: (nil)", d->age);
 	printf(d->owner != NULL ? "Owner: %s": "Owner: (nil)", d->owner);
 }
