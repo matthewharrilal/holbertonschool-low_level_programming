5771d3f65cebca46a9a9df119fcf25e8122bb1c4
matthewharrilal <matthewharrilal@gmail.com>
2023-08-21 06:27:45 -0700

    Returning first index that cmp does not return 0

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include  "main.h"
+
+#include <stdio.h>
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+	if (size <= 0)
+	{
+		return -1;
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+		{
+			return index;
+		}
+	}
+
+	return -1;
+}
