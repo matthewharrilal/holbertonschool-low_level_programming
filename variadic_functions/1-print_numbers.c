cee4d3da98d81b144152c30c821b40e698b1a89b
matthewharrilal <matthewharrilal@gmail.com>
2023-08-21 10:58:43 -0700

    Added method that prints all the arguments in a variadic method

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int index;
+	va_list args;
+	
+	va_start(args, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator != NULL)
+		{
+			printf("%c", separator);
+		}
+	}
+	printf("\n");
+}
