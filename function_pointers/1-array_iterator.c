5458240b301c4452280293d1b7e5fbdda8aba94a
matthewharrilal <matthewharrilal@gmail.com>
2023-08-21 06:21:02 -0700

    Added method that performs action on array each iteration

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+#include <stdio.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t index;
+	if (action == NULL)
+	{
+		return;
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		action(array[index]);
+	}
+}
