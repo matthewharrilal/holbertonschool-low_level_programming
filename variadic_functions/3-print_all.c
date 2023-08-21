9e8cf34f962e3d523e07916196fd05b201b4b2d1
matthewharrilal <matthewharrilal@gmail.com>
2023-08-21 11:51:12 -0700

    Does this meet all the requirements

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+
+#include <stdio.h>
+
+void print_all(const char * const format, ...)
+{
+	int index; 
+	va_list args;
+	char *s;
+	int i;
+	float f;
+	char c;
+
+	va_start(args, format);
+
+	for (index = 0; format[index] != '\0'; index++)
+	{
+		if (format[index] == "c")
+		{	
+			c = va_arg(args, char);
+			printf("%c", c);
+		} else if (format[index] == "i")
+		{
+			i = va_arg(args, int);
+			printf("%d", i);
+		} else if (format[index] == "f")
+		{
+			f = va_arg(args, float);
+			printf("%f", f);
+		} else if (format[index] == "s")
+		{
+			s = va_arg(args, char *);
+			if (s != NULL)
+			{
+				printf("%s", s);	
+			} else
+			{
+				printf("(nil)");
+			}
+		}
+	}
+}
