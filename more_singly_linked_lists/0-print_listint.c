518db6746112d2183a5577e4e0038b9fa7eaf977
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 09:02:02 -0700

    Incrementing opertor and fixed naming issue

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@ size_t print_listint(const listint_t *h)
 	const listint_t *current;
 	size_t counter;
 
-	current = head;
+	current = h;
 	counter = 0;	
 
 	while (current)
 	{
 		printf("%d\n", current->n );
 		counter++;
+		current = current->next;
 	}
 
 	return counter;
