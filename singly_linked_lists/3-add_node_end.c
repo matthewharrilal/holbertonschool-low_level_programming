6d21cfd03f74967e01b20ab0f35e6bd1bfdc7d8f
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 08:01:54 -0700

    Initial commit of adding a node to the end of a list

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *strDuplicate;
+	list_t *newNode;
+	list_t *current;
+
+	strDuplicate = strdup(str);
+	if (strDuplicate == NULL)
+	{
+		free(strDuplicate);
+		return NULL;
+	}
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		free(newNode);
+		return NULL;
+	}
+
+	newNode->str = strDuplicate;
+	newNode->len = strlen(strDuplicate);
+	newNode->next = NULL;
+
+	current = *h;
+	while (current)
+	{
+		current = current->next;
+	}
+
+	current->next = newNode;
+	return newNode;
+}
