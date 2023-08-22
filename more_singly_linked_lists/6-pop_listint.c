ace8b700f83d87308e363ef2759bc21a70052cf4
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 11:27:12 -0700

    Initial commit of deleting the first node in the linked list

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	*temp = *head;
+
+	if (*head == NULL)
+	{
+		return 0;
+	}
+
+	*head = temp->next;
+	temp = NULL;
+}
