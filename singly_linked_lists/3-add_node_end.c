2a19efb244a49d7a1d0b3de6ae71095c20433999
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 08:03:05 -0700

    Initial commit of adding a node to the end of a list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -30,7 +30,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	newNode->len = strlen(strDuplicate);
 	newNode->next = NULL;
 
-	current = *h;
+	current = *head;
 	while (current)
 	{
 		current = current->next;
