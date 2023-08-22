6eead620a116f5900f80336a2761d834b95e6b08
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 11:52:58 -0700

    Added prev pointer?

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -8,6 +8,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current;
 	listint_t *newNode;
 	listint_t *temp;
+	listint_t *prev;
 	unsigned int counter;
 
 	if (*head == NULL || head == NULL)
@@ -24,17 +25,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode->n = n;
 	
 	current = *head;
+	prev = *head;
 
 	while (current->next)
 	{
 		if (counter == idx)
 		{
-			temp = current->next;
-			current->next = newNode;
+			temp = current;
+			prev->next = newNode;
 			newNode->next = temp;
 		}
 
 		counter++;
+		prev = current;
 		current = current->next;
 	}
 
