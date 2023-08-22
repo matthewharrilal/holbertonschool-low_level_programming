4b615316ac5c36ddc289013d3e9198b2b5e70996
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 09:46:31 -0700

    Properly freeing the linked list?

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+
+
+	while (*head)
+	{
+		temp = *head;
+		*head = *head->next;
+		free(temp);
+	}
+
+	head = NULL;
+}
