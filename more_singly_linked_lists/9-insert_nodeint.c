017c89911113400c9218fb6b6c91230a20a76c9a
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 11:48:37 -0700

    Initial commit of inserting node at nth index

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current;
+	listint_t *newNode;
+	listint_t *temp;
+	int counter;
+
+	if (*head == NULL || head == NULL)
+	{
+		return NULL;
+	}
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+	{
+		return NULL;
+	}
+
+	newNode->n = n;
+	
+	current = *head;
+
+	while (current->next)
+	{
+		if (counter == idx)
+		{
+			temp = current->next;
+			current->next = newNode;
+			newNode->next = temp;
+		}
+
+		counter++;
+		current = current->next;
+	}
+
+	return newNode;
+}
