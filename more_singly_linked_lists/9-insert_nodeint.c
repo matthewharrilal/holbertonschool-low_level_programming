364d5ee6eecdb8d6450e65046cfb5018a5d7bad8
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 12:29:59 -0700

    Returned new node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = *head;
 		*head = newNode;
 		newNode->next = temp;
+		return newNode;
 	}
 
 	while (current)
