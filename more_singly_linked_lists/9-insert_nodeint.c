f22a3dbe5fff8748d34db5a8b0e3ffd08dd3d303
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 12:32:39 -0700

    Handling case if we want to insert node after last index and not at the last index

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -51,6 +51,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 
+	if (counter == idx)
+	{
+		current->next = newNode;
+		new_node->next = NULL;
+		return newNode;
+	}
+
 	free(newNode);
     	return NULL;
 }
