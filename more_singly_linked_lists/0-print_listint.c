96a1f93eaef1cd924881bc5cd18d70aa4da2db33
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 09:00:25 -0700

    Printing elements of linked list and returning count of elements

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current;
+	size_t counter;
+
+	current = head;
+	counter = 0;	
+
+	while (current)
+	{
+		printf("%d\n", current->n );
+		counter++;
+	}
+
+	return counter;
+}
