bff559fd9aa0532eb2b6c15df825a497484a08a5
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 07:41:12 -0700

    Prepending/updating the new head of a linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+list_t *add_node(list_t **head, const char *str)
+{
+	const list_t *newNode;
+	char *strDuplicate;
+
+	newNode = malloc(sizeof(list_t));
+	strDuplicate = strdup(str);
+	
+	if (newNode == NULL || strDuplicate == NULL)
+	{
+		return NULL;
+	}
+
+	newNode->str = strDuplicate;
+	newNode->len = strlen(strDuplicate);
+	newNode->next = *head;
+
+	*head = newNode;
+
+	return newNode;
+}
