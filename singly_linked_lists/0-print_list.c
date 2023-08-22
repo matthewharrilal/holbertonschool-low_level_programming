0591f7a829d49e412b8425a1d763cda6e4de2078
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 06:31:37 -0700

    Added a program that prints the content of linked list ins epcified format

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+size_t print_list(const list_t *h)
+{
+	size_t counter;
+
+	counter = 0;
+	/* While the current node exists  */
+	while (*h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		} else
+		{
+			printf("[%d] %s", h->len, h->str);
+		}
+		h = h->next;
+		counter++;
+	}
+
+	return counter;
+}
