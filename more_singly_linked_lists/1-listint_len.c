d6321706bf9806a9f86c8617b43a1f02f96aa83f
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 09:05:12 -0700

    Very similar to previous method

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter;
+	const listint_t current;
+
+	counter = 0;
+	current = h;
+
+	while (current)
+	{
+		counter++;
+		current = current->next;
+	}
+
+	return counter;
+}
