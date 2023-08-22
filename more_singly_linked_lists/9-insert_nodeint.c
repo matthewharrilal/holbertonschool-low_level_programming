8ded3ededb6d11df773423ba8c20d7f8223997a7
matthewharrilal <matthewharrilal@gmail.com>
2023-08-22 12:10:21 -0700

    Dereferencing the first level of head

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		temp = head;
+		temp = *head;
 		*head = newNode;
 		newNode->next = temp;
 	}
