8d6afb3c0477502b78ed3f1807af3afd5444654b
matthewharrilal <matthewharrilal@gmail.com>
2023-08-23 06:03:00 -0700

    Initial commit of deleting node in linked list at specific index

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,60 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+	listint_t *prev;
+	listint_t *current;
+	listint_t *next;
+
+	unsigned int counter;
+
+	if (index < 0)
+	{
+		return -1
+	}
+
+	prev = *head;
+	current = *head;
+	counter = 0;
+
+	if (index == 0)
+	{
+		if (current->next)
+		{
+			*head = current->next;
+			free(current);
+			free(prev);
+		} else {
+			*head = NULL;
+		}
+	}
+
+	while (current->next)
+	{
+
+		if (counter == index)
+		{ 
+			next = current->next;
+			prev->next = next;
+			return 1;
+		}
+		
+		prev = current;
+		current = current->next;
+		counter++;
+	}
+
+	
+	if (counter == index)
+	{
+		prev->next = NULL;
+		free(current);
+	}
+
+	return -1;
+}
