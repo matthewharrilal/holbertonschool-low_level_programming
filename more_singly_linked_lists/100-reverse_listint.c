6d3a989a0b432c1ea96020aad9656318fc2ed78f
matthewharrilal <matthewharrilal@gmail.com>
2023-08-23 06:54:18 -0700

    Added logic for reversing linked list in place

---
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev;
+	listint_t *current;
+
+	if (head == NULL || *head == NULL)
+	{
+		return NULL;
+	}
+
+	prev = NULL;
+	current = *head;
+	while (current->next)
+	{
+		*head = current->next /* We utilize head as our temp to save reference to the rest of the list  */
+		current->next = prev;
+
+		prev = current;
+		current = *head;
+	}
+
+	current->next = prev;
+	*head = current;
+	return prev;
+}
+
+
+/* This approach is if we can allocate three variables
+ *
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev;
+	listint_t *current;
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+	{
+		return NULL;
+	}
+
+	prev = NULL;
+	current = *head;
+	while (current->next)
+	{
+		temp = current->next;
+		current->next = prev;
+
+		prev = current;
+		current = temp;
+	}
+
+	* When we exit out of the while loop current will be null therefore prev holds our last node in the original list (head of the new reversed list)   *
+	*head = prev;
+	return prev;
+} */
