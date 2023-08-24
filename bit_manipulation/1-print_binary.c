f0c8c7b5b417ba2b05e865ef68d2a3516ea193ea
matthewharrilal <matthewharrilal@gmail.com>
2023-08-24 09:03:13 -0700

    Initial commit of representing integer in binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+void print_binary(unsigned long int n)
+{
+	int index;
+	unsigned long int *dynamicArray;
+
+	if (n == 1)
+	{
+		putchar('1');
+		return;
+	} else if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+
+
+	index = 0;
+	dynamicArray = malloc(sizeof(unsigned long int));
+
+
+	while (num > 0)
+	{
+		dynamicArray[index] = (n % 2); /* Every iteration we divide by 2 and that shifts us to the left as we transition left value should get smaller  */
+		n /= 2;
+	}
+
+	printf("%p\n", (void *)dynamicArray);
+	return;
+}
