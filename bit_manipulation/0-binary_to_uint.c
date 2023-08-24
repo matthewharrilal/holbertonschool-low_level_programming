3473d001c1264839e1959dc9178f27b047b9cf13
matthewharrilal <matthewharrilal@gmail.com>
2023-08-24 08:06:10 -0700

    Made additional changes

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 		return 1;
 	}
 	index = 0;
-	placeCounter = (strlen(b) - 1) * 2; /* Used to deterimine the max eponent we raise to given we read left to right  */
+	placeCounter = 2 ^ (strlen(b) - 1); /* Used to deterimine the max eponent we raise to given we read left to right  */
 	sum = 0;
 
 	while (b[index])
