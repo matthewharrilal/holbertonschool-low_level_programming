ee3292b3a93b33a887c7419e689abe13d95d71fa
matthewharrilal <matthewharrilal@gmail.com>
2023-08-24 07:22:55 -0700

    Initial commit of converting binary to unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	int index;
+	int placeCounter;
+	unsigned int sum;
+
+	index = 0;
+	placeCounter = 32;
+	sum = 0;
+
+	while (b[index])
+	{
+		if (atoi(b[index]) == 1)
+		{
+			sum += (b[index])^ placeCounter;		
+		} else if (atoi(b[index]) != 0)
+		{
+			return 0;
+		}
+
+		index++;
+		placeCounter /= 2;
+	}
+
+	return sum;
+}
