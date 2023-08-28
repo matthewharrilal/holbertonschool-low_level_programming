140a339b5347d16ab8b4113240d98dc85cd91d7e
matthewharrilal <matthewharrilal@gmail.com>
2023-08-28 13:18:14 -0700

    Returning -1 if index is out of range

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,6 +1,12 @@
 #include "main.h"
+#include <limits.h>
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index == ULONG_MAX)
+	{
+		return -1;
+	}
+		
 	return n & (1UL << (index)) ? 1 : 0;
 }
