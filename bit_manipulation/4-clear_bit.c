d36c8bfbccd4431e2051a55f7600b3de49839c46
matthewharrilal <matthewharrilal@gmail.com>
2023-08-29 09:07:07 -0700

    Added error checking conditional

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -4,6 +4,11 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
+	if (index > (CHAR_BIT * sizeof(unsigned long int)))
+	{
+		return -1;
+	}
+
 	mask = ~(1UL << index);
 	*n = *n & mask;
 	return 1;
