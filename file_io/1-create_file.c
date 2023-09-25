58bd6ce73b9f6152a2b4f2b2d7db9e5224dea319
matthewharrilal <matthewharrilal@gmail.com>
2023-09-25 11:23:15 -0700

    Using strlen

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return -1;
 	}
 
-	buffer = malloc(sizeof(char *));
+	buffer = malloc(strlen(text_content) + 1);
 	if (buffer == NULL)
 	{
 		close(fd);
