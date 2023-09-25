ea1adbdbf580d74100029cd59222a803d07fe1a0
matthewharrilal <matthewharrilal@gmail.com>
2023-09-25 12:39:37 -0700

    Omitted the const keyword

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -43,7 +43,7 @@ int create_file(const char *filename, char *text_content)
 }
 */
 
-int create_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
     int fd;
     ssize_t bytes_written;
