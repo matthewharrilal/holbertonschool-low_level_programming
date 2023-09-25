70e2cc7b0ceb383ae2db5e17843d99a51d450af5
matthewharrilal <matthewharrilal@gmail.com>
2023-09-25 12:36:34 -0700

    Added another arg when creating the file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	/* Create the file with the specific permissions */
-	fd = open(filename, O_CREAT, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 
 	if (fd == -1)
 	{
