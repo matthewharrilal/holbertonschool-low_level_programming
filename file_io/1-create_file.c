bc27ec5a1a8c8bae1ea8d6190178bd945b2ffc20
matthewharrilal <matthewharrilal@gmail.com>
2023-09-25 11:16:47 -0700

    Added method to write text content to file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	char buffer;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return -1;
+	}
+
+	/* Create the file with the specific permissions */
+	fd = open(filename, O_CREAT, 600);
+
+	if (fd == -1)
+	{
+		return -1;
+	}
+
+	buffer = malloc(text_content);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return -1;
+	}
+
+	bytes_written = write(filename, buffer, strlen(text_content));
+	
+	if (bytes_written == -1)
+	{
+		close(fd);
+		free(buffer);
+		return -1;
+	}
+
+
+	return 0;
+	
+}
