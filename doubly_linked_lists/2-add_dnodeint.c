53dbd0bf1d4e0a75fdee9f5040e3bcdd556348a8
matthewharrilal <matthewharrilal@gmail.com>
2023-10-02 09:46:11 -0700

    malloc before assingment

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,17 +4,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
 
-	if (head == NULL || *head == NULL)
-	{
-		*head = newNode;
-		return newNode;
-	}
-
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 	{
 		exit(98);
 	}
+
+	if (head == NULL || *head == NULL)
+        {
+		newNode->n = n;
+                *head = newNode;
+                return newNode;
+        }
 
 	newNode->n = n;
 
