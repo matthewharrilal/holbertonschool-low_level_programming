707a2b64c0f20a4e06001fefd98ad562acb17f2e
matthewharrilal <matthewharrilal@gmail.com>
2023-10-02 11:39:05 -0700

    Never setting n as the nodes data

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,6 +19,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		current = current->next;
 	}
 
+	newNode->n = n;
 	newNode->prev = current;
 	current->next = newNode;
 	newNode->next = NULL;
