d7367d43d8f0d24f2b67744628a6839dd3dfc63d
matthewharrilal <matthewharrilal@gmail.com>
2023-10-02 09:20:08 -0700

    Added method to count the number of elements in a doubly linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t counter;
+
+	current = h;
+	counter = 0;
+
+	while (current)
+	{
+		counter++;
+		current = current->next;
+	}
+
+	return counter;
+}
