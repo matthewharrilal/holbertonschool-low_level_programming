62049de89b40c7f7eef979980e6be0dfbce77ee4
matthewharrilal <matthewharrilal@gmail.com>
2023-10-03 06:26:29 -0700

    Freeing current

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -3,12 +3,15 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current;
+	dlistint_t *next;
 
 	current = head;
 	while (current)
 	{
+		next = current->next;
 		free(current);
-		current = current->next;  
+		current->prev = NULL;
+		current = next;  
 	}
 
 	free(head);
