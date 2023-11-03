483e3317e0253c36c75398ece9b87a7faedf84c4
Matthew Harrilal <matthewharrilal@Matthews-MacBook-Pro.local>
2023-11-03 08:41:24 -0500

    Wrote program to add the values of all nodes in dll

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+
+	int sum;
+	dlistint_t *current;
+
+
+	if (head == NULL)
+	{
+		return 0
+	}
+
+	current = head;
+	sum = 0
+
+
+	while (current)
+	{
+
+		sum += current->n;
+		current = current->next;
+	}
+
+	return sum;
+}
