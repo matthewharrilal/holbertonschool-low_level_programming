fa6675cf48debb4fa0c06e677d87facc96e5ff1a
Matthew Harrilal <matthewharrilal@Matthews-MacBook-Pro.local>
2023-11-06 12:08:15 -0600

    Testing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -69,7 +69,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		prev->next = newNode;
 		newNode->prev = prev;
 		newNode->next = NULL;
-		return newNode;
+		return NULL;
 	}
 
 	return newNode;
