4605fb13a45d573da85d27472ca9c8fac2927b7e
Matthew Harrilal <matthewharrilal@Matthews-MacBook-Pro.local>
2023-11-06 10:53:21 -0600

    Initializing current index to 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	current = *h;
 	newNode = malloc(sizeof(dlistint_t));
-	
+	currentIndex = 0;	
+
 	if (newNode == NULL)
 	{
 		return NULL;
