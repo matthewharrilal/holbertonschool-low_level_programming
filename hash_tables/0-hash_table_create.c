739e429f93f142c656ef29db3ed3ce0df727e5c8
Matthew Harrilal <matthewharrilal@Matthews-MacBook-Pro.local>
2023-11-13 13:00:29 -0600

    Wasn't mallocing space for the array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-
+	unsigned lont int index;
 
 	hash_table = malloc(sizeof(hash_table_t));
 
@@ -14,7 +14,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	hash_table->size = size;
-	hash_table->array = NULL;
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return NULL;
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		hash_table->array[index] = NULL;
+	}
 
 	return hash_table;
 
