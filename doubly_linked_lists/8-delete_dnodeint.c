e4d7b2ec270d9cf5da1f3934574c70d646f9e757
Matthew Harrilal <matthewharrilal@Matthews-MacBook-Pro.local>
2023-11-13 10:49:20 -0600

    Updating prev

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -25,6 +25,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	while (current != NULL)
 	{
+		
+		prev = current;
 
 		if (index == counter)
 		{
@@ -36,7 +38,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return 1;
 		}
 	
-		prev = current;
 		current = current->next;
 		counter++;		
 	}
