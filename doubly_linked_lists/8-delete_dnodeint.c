8b38ecd8ded844ba2c23a0e36bd41d3e29f46196
Matthew Harrilal <matthewharrilal@Matthews-MacBook-Pro.local>
2023-11-13 10:29:49 -0600

    Added code to delete node at the end of doubly linked list

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int counter;
+	dlistint_t *prev;
+
+	if (*head == NULL || head == NULL)
+	{
+		return -1;
+	}
+
+	counter = 0;
+	current = *head;
+
+	while (current != NULL)
+	{
+		prev = current->prev;
+
+		if (index == counter)
+		{
+			prev->next = current->next;
+			
+			current->prev = NULL;
+			current->next = NULL;
+			free(current);
+			return 1;
+		}
+	
+		current = current->next;
+		counter++;		
+	}
+
+
+	if (index == counter)
+	{
+		prev->next = NULL;
+		return 1;
+	}
+
+	return -1;
+}
